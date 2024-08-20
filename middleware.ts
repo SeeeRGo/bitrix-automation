a3abbf968ed0a756023e1b2917e7e1aa84b6eaba
Мизин Сергей Юрьевич (4091265) <mizin@vtb.ru>
2024-08-20 13:48:56 +0400

    session fixes

--- middleware.ts
+++ middleware.ts
@@ -12,7 +12,7 @@ export default async function middleware(req: NextRequest) {
   const isPublicRoute = publicRoutes.includes(path)
  
   // 3. Decrypt the session from the cookie
-  const cookie = cookies().get('authjs.session-token')?.value
+  const cookie = cookies().get('__Secure-authjs.session-token')?.value
   const session = cookie  
  
   // 5. Redirect to /login if the user is not authenticated
