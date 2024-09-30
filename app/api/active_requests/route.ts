21776635ba11a4b618ab877b7ff3c490a3175768
Мизин Сергей Юрьевич (4091265) <mizin@vtb.ru>
2024-09-30 14:34:50 +0400

    removed logs

--- app/api/active_requests/route.ts
+++ app/api/active_requests/route.ts
@@ -4,12 +4,9 @@ const activeRequestsColumn = 'C21:UC_NGHX3A'
 export const revalidate = 0
 export async function GET() {
                   
-  const list = await axios.get(`${process.env.BITRIX_WEBHOOK}/crm.deal.list?filter[STAGE_ID]=${activeRequestsColumn}&select[]=*&select[]=UF_*`).then(({ data }) => {    
-    console.log('data', data);
-    
+  const list = await axios.get(`${process.env.BITRIX_WEBHOOK}/crm.deal.list?filter[STAGE_ID]=${activeRequestsColumn}&select[]=*&select[]=UF_*`).then(({ data }) => {        
     return data.result;
   })  
-  console.log('list', list);
   
  
   return Response.json(list)
