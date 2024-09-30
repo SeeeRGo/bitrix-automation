62f212c3455be9d73ec9b1908aaad865e07e258d
Мизин Сергей Юрьевич (4091265) <mizin@vtb.ru>
2024-09-30 13:27:32 +0400

    revalidating answers const set

--- app/api/active_requests/route.ts
+++ app/api/active_requests/route.ts
@@ -1,12 +1,16 @@
 import axios from "axios"
 
 const activeRequestsColumn = 'C21:UC_NGHX3A'
-
+export const revalidate = 0
 export async function GET() {
                   
   const list = await axios.get(`${process.env.BITRIX_WEBHOOK}/crm.deal.list?filter[STAGE_ID]=${activeRequestsColumn}&select[]=*&select[]=UF_*`).then(({ data }) => {    
+    console.log('data', data);
+    
     return data.result;
   })  
+  console.log('list', list);
+  
  
   return Response.json(list)
 }
